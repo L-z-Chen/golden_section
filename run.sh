6998997bf030686615c278d3926a6d6f60462c9b
Li Junbo (jl77863) <jl77863@austin.utexas.edu@ece-a51939.austin.utexas.edu>
2025-03-23 18:31:04 -0500

    updated, you can pick number of gpus and batchsizes, local batch and global batch and do gradient accumulations

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export WANDB_API_KEY=9533feefe54c8861797f495e7872fa9631fe5a77
+export WANDB_API_KEY=<YOUR_WANDB_API_KEY>
 export CUDA_VISIBLE_DEVICES=0
 torchrun --nproc_per_node=1 --master-port=29502 /home/jl77863/muon/main.py \
                                                         --local_batch_size 24 \
